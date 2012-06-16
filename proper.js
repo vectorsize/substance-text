4ded67b3bb2d72cfaed368145f1543e20a32df3c
Victor Saiz <vectorsize@gmail.com>
2012-06-16 17:07:29 +0200

    implementing the API

--- proper.js
+++ proper.js
@@ -13,160 +13,104 @@
 //   all these platforms (if possible).
 
 (function(){
+  
+  // Proper
+  // ------
 
-  var Range = Backbone.Model.extend({});
-  var Doc = Backbone.Collection.extend({
-        model: Range
-  });
+  this.Proper = function(options){
 
-  this.Proper = function(options) {
+    var range
+      , annotationList = []
+      , options = options || {};
 
-    var doc = new Doc()
-    ,   from
-    ,   to
-    ,   combined
-    ,   fit
-    ,   commands = {
-          em: {
-            name: 'Emphasis',
-            command: 'em',
-            key: '(CTRL+SHIFT+E)',
-            isActive: false,
-            exec: function() {
-              exec('em');
-            }
-          },
-          strong: {
-            name: 'Strong',
-            command: 'strong',
-            key: '(CTRL+SHIFT+S)',
-            isActive: false,
-            exec: function() {
-              exec('strong');
-            }
-          },
-          underline: {
-            name: 'Underline',
-            command: 'underline',
-            key: '(CTRL+SHIFT+U)',
-            isActive: false,
-            exec: function() {
-              exec('underline');
-            }
-          }
-    };
-
-    // initialize CodeMirror
-    var editor = CodeMirror.fromTextArea($(".editable")[0], {
-      lineNumbers: false,
+    var defaults = { 
       lineWrapping: true,
-      onCursorActivity: function() {
-        from = editor.getCursor(true);
-        to = editor.getCursor(false);
-        combined = '' + from.ch + '' + from.line + '' + to.ch + '' + to.line + '';
-        matchRanges();
-      }
-    });
-
-    // initialize Tools
-    var initToolbar = function(){
-      
-      var toolBTN = _.template(
-        '<li>\
-            <a href="#<%= command %>" title="<%= name %> <%= key %>" class="command <%= command %>" id="<%= command %>" command="<%= command %>">\
-              <div><%= name %></div>\
-            </a>\
-          </li>'
-        );
-      
-      _.each(commands, function(com){
-        var tool = $(toolBTN({'command':com.command, 'name':com.name, 'key':com.key}));
-
-        tool.click(function(event) {
-          event.preventDefault();
-          commands[com.command].exec(com.command);
-        });
-
-        $('#tools').append(tool);
-      });
+      lineNumbers: false
     };
 
-    // Adds a new range to the document and applies the visual styles to the html
-    var exec = function(type){
-      
-      if(editor.getSelection().length > 0){
-        trim();
-        var current = new Range({'from':from, 'to':to, 'type':type, 'combined': combined});
+    var overrides = {
+      lineWrapping: true,
+      onCursorActivity: function(cm){
+        // Set the selection range
+        var from = cm.getCursor(true);
+        var to = cm.getCursor(false);
+        var str = cm.getSelection();
+        range = {'from':from, 'to':to, 'str': str};
+      }
+    };
 
-        //a range matches
-        if(typeof fit !== 'undefined'){
-          // it's not of the same type we take away the xisting one to replace it
-          if(fit.get('type') !== type){
-            doc.remove(fit);
-            doc.add(current);
-          }else{
-            // it's of the same type we deactivate/take away the xisting one
-            doc.remove(fit);
-          }
-          
-        }else{
-        //no matching ranges we just add it
-          doc.add(current);
+    // Merge options
+    _.extend(defaults, options);
+    _.extend(options, overrides);
+
+    // Init codeMirror
+    var cm = CodeMirror.fromTextArea($(options.el)[0], options);
+
+    // Resets the cursor selection to the actual range
+    function restetCursor(range){
+      // Reselect trimmed range and string value
+      cm.setSelection(range.from, range.to);
+    }
+
+    // Unselects left or right blank characters
+    function trim(sel){
+      var first = sel.str.charCodeAt(0);
+      var last = sel.str.charCodeAt(sel.str.length-1);
+      // Check if first character is blank and adjusts the annotation range if it is
+      if(first === 32){
+        sel.from.ch = sel.from.ch + 1;
+      }
+
+      // Check if last character is blank and adjusts the annotation range if it is
+      if(last === 32){
+        sel.to.ch = sel.to.ch - 1;
+      }
+
+      sel.str = cm.getRange(sel.from, sel.to);
+      return sel;
+    }
+
+    // Turns position line objects into a single value representing the offset from character 0
+    function toOffset(pos){
+      var offset = 0;
+      if(pos.line > 0){
+        var i = 0;
+        for(; i < pos.line; i++ ){
+          offset += cm.lineInfo(i).text.length + 1;//count the newline character as 1?
         }
-
-        editor.replaceRange(editor.getValue() , {line:0, ch:0}, {line:editor.lineCount(), ch:'0'});
-        
-        _.each(doc.models, function(model){
-          var from = model.get('from')
-          ,   to = model.get('to')
-          ,   type = model.get('type');
-
-          editor.markText(from, to, type);
-        });
-
-        $('#source').html(JSON.stringify(doc.toJSON()));
-        editor.setSelection(current.get('from'), current.get('to'));
       }
+      return pos.ch + offset;
+    }
+    
+    // Returns the current selection
+    function selection(){
+      range = trim(range);
+      var start = toOffset(range.from);
+      var end = toOffset(range.to);
+      return {'start': start, 'end': end};
+    }
+
+    // Adds annotation (uses the current selection)
+    function annotate(note){
+      note.pos = selection();
+      //autogenerated id based on the type and the ofsset position string
+      note.id = note.type + '/' + note.pos.start + '' + note.pos.end;
+      restetCursor(range);
+      annotationList.push(note);
+    }
+
+    // Returns list of annotations
+    var annotations = function(){
+      return annotationList;
     };
 
-    //quantize the selection to the nearest non-blank character
-    var trim = function(){
-
-      var head = editor.getRange(from, {'ch':from.ch + 1, 'line':from.line});
-      var tail = editor.getRange({'ch':to.ch - 1, 'line':to.line}, to);
-
-      if(head.charCodeAt(0) === 32){
-        from.ch = from.ch + 1;
-      }
-      
-      if(tail.charCodeAt(0) === 32){
-        to.ch = to.ch - 1;
-      }
-      
-      editor.setSelection(from, to);
+    //API
+    return {
+      selection: selection,
+      annotate: annotate,
+      annotations: annotations
     };
-
-    //checks if the selection is already marked and activates the corresponding tool
-    var matchRanges = function(){
-      var match = doc.where({'combined':combined});
-      fit = match[0];
-
-      if(match.length > 0){
-          commands[fit.get('type')].isActive = true;
-          $('#' + fit.get('type')).addClass('selected');
-      }else{
-        _.each(commands, function(com, key){
-          commands[key].isActive = false;
-        });
-        $('ul#tools li a').removeClass('selected'); 
-      }
-
-    };
-
-    initToolbar();
-
-    // Expose public API
-    // -----------------
-    //return {};
   };
+   
+
 })();
